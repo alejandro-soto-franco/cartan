50c9d029564690dea2307f00619c9872624c9635
alejandrosotofranco <soto.alejandro2025@gmail.com>
2026-04-17 14:07:02 +0300

    chore: untrack cartan-py/target and ignore nested target/ dirs

--- cartan-py/target/debug/build/num-traits-404689dd1f8df129/build_script_build-404689dd1f8df129.d
+++
@@ -1,5 +0,0 @@
-/home/alejandrosotofranco/cartan/cartan-py/target/debug/build/num-traits-404689dd1f8df129/build_script_build-404689dd1f8df129.d: /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/alejandrosotofranco/cartan/cartan-py/target/debug/build/num-traits-404689dd1f8df129/build_script_build-404689dd1f8df129: /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs:
--- cartan-py/target/debug/deps/once_cell-dc700a0db4d40c33.d
+++
@@ -1,7 +0,0 @@
-/home/alejandrosotofranco/cartan/cartan-py/target/debug/deps/once_cell-dc700a0db4d40c33.d: /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/lib.rs /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/imp_std.rs /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/race.rs
-
-/home/alejandrosotofranco/cartan/cartan-py/target/debug/deps/libonce_cell-dc700a0db4d40c33.rmeta: /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/lib.rs /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/imp_std.rs /home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/race.rs
-
-/home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/lib.rs:
-/home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/imp_std.rs:
-/home/alejandrosotofranco/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.4/src/race.rs:
