243ba8248d1f8bcd49c3c2cd2e51a50c105f01b3
alejandrosotofranco <soto.alejandro2025@gmail.com>
2026-06-22 19:17:21 +0300

    feat(cartan-simplicial): port abstract simplex and subsimplex combinatorics

---
+++ cartan-simplicial/src/topology.rs
@@ -0,0 +1,7 @@
+//! Abstract simplicial topology.
+//! Ported from luiswirth/formoniq (used with permission), adapted for cartan.
+
+/// Index of a vertex in a mesh.
+pub type VertexIdx = usize;
+
+pub mod simplex;
