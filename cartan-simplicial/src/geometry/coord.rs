45da9c1405d3b50a2f3d0b282b74c7a5022c692b
alejandrosotofranco <soto.alejandro2025@gmail.com>
2026-06-22 19:35:29 +0300

    feat(cartan-simplicial): port simplex quadrature rules

--- cartan-simplicial/src/geometry/coord.rs
+++ cartan-simplicial/src/geometry/coord.rs
@@ -2,9 +2,11 @@
 
 pub mod simplex;
 pub mod mesh;
+pub mod quadrature;
 
 use crate::linalg::{RowVector, Vector};
 
 pub type Coord = Vector;
 pub type TangentVector = Vector;
 pub type CoTangentVector = RowVector;
+pub type CoordRef<'a> = &'a nalgebra::DVectorView<'a, f64>;
